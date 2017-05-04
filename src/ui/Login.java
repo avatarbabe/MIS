62e2ffe456296206d889d85b99f087885c5b0a2a
avatarbabe <marso083@student.liu.se>
2017-05-04 13:38:29 +0200

    Loginruta

---
+++ src/ui/Login.java
@@ -0,0 +1,12 @@
+package ui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class Login extends JPanel{
+	
+	public Login(){
+		setPreferredSize(new Dimension(400, 400));
+	}
+}
--- src/ui/Misma.java
+++ src/ui/Misma.java
@@ -1,10 +1,29 @@
 package ui;
 
-public class Misma {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Misma extends JFrame {
+
+	public Misma() {
+		setTitle("MISMA");
+		setPreferredSize(new Dimension(400, 400));
+		loadLogin();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	private void loadLogin() {
+		getContentPane().removeAll();
+		JPanel login = new Login();
+		add(login);
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Misma mis = new Misma();
 	}
 
 }
