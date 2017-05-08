5bb4940504f69ecf4d33a20a32e1f2bb17b072f9
Linnea <Linnea.f.h@gmail.com>
2017-05-08 14:26:38 +0200

    Registerroutes ui

---
+++ src/ui/RegisterRoutes.java
@@ -0,0 +1,44 @@
+package ui;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class RegisterRoutes extends JPanel{
+	
+	public RegisterRoutes(){
+		setPreferredSize(new Dimension(400, 400));
+		setFocusable(true);
+		
+		setLayout(new BoxLayout(this, 1));
+		
+		JTextField from = new JTextField();
+		JTextField to = new JTextField();
+		JTextField distance = new JTextField();
+		JButton regitster = new JButton();
+		
+		from.setText("From");
+		from.setAlignmentX(CENTER_ALIGNMENT);
+		from.setMaximumSize(new Dimension(150, 20));
+		to.setText("To");
+		to.setAlignmentX(CENTER_ALIGNMENT);
+		to.setMaximumSize(new Dimension(150, 20));
+		distance.setText("Distance");
+		distance.setAlignmentX(CENTER_ALIGNMENT);
+		route.setMaximumSize(new Dimension(150, 20));
+		login.setText("Register");
+		login.setAlignmentX(CENTER_ALIGNMENT);
+		
+		add(Box.createRigidArea(new Dimension(200, 100)));
+		add(from);
+		add(to);
+		add(distance);
+		add(Box.createRigidArea(new Dimension(200, 15)));
+		add(register);
+	}
+}
