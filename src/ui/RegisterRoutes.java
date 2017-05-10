26497c14586ace6c30f406bb67b04e09a6424e98
Linnea <Linnea.f.h@gmail.com>
2017-05-10 15:10:45 +0200

    dropdown

--- src/ui/RegisterRoutes.java
+++ src/ui/RegisterRoutes.java
@@ -8,6 +8,8 @@ import java.awt.event.MouseEvent;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,13 +18,17 @@ public class RegisterRoutes extends JPanel{
 	public RegisterRoutes(Misma misma){
 		setPreferredSize(new Dimension(400, 400));
 		setFocusable(true);
-		
 		setLayout(new BoxLayout(this, 1));
+		String[] message = {"Truck x2000", "Truck 900", "Truck 30s"};
 		
+		JLabel vehicle = new JLabel("Vehicle:");
 		JTextField from = new JTextField();
 		JTextField to = new JTextField();
 		JTextField distance = new JTextField();
+		JComboBox box  = new JComboBox(message);
 		JButton register = new JButton("Register");
+		
+		
 		
 		from.setText("From");
 		from.addMouseListener(new MouseAdapter() {
@@ -33,6 +39,7 @@ public class RegisterRoutes extends JPanel{
 		});
 		from.setAlignmentX(CENTER_ALIGNMENT);
 		from.setMaximumSize(new Dimension(150, 20));
+		
 		to.setText("To");
 		to.addMouseListener(new MouseAdapter() {
 		    @Override
@@ -42,6 +49,7 @@ public class RegisterRoutes extends JPanel{
 		});
 		to.setAlignmentX(CENTER_ALIGNMENT);
 		to.setMaximumSize(new Dimension(150, 20));
+		
 		distance.setText("Distance");
 		distance.addMouseListener(new MouseAdapter() {
 		    @Override
@@ -51,12 +59,25 @@ public class RegisterRoutes extends JPanel{
 		});
 		distance.setAlignmentX(CENTER_ALIGNMENT);
 		distance.setMaximumSize(new Dimension(150, 20));
+		vehicle.setAlignmentX(CENTER_ALIGNMENT);
+		
+		box.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		        distance.setText("");
+		    }
+		});
+		box.setAlignmentX(CENTER_ALIGNMENT);
+		box.setMaximumSize(new Dimension(150, 20));
+		
 
 		register.setAlignmentX(CENTER_ALIGNMENT);
 		add(Box.createRigidArea(new Dimension(200, 100)));
 		add(from);
 		add(to);
 		add(distance);
+		add(vehicle);
+		add(box);
 		add(Box.createRigidArea(new Dimension(200, 15)));
 		add(register);
 		
