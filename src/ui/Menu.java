b513fd93547909501322ec30b836b7efb8093390
Linnea <Linnea.f.h@gmail.com>
2017-05-11 11:44:18 +0200

    image

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -3,8 +3,10 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -19,23 +21,27 @@ public class Menu extends JPanel{
 	private JButton menu3 = new JButton("Edit tax");
 	private JButton menu4 = new JButton("View emissions");
 	private JButton menu5 = new JButton("Log out");
+	private JButton goBack = new JButton(new ImageIcon(getClass().getClassLoader().getResource("images/arrow.png")));
 	
 	public Menu(Misma misma, int level) {
 		setLayout(new BoxLayout(this, 1));
 		setPreferredSize(new Dimension(400, 400));
 		setFocusable(true);
 		add(Box.createRigidArea(new Dimension(200, 100)));
+		goBack.setVisible(true);
+		goBack.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+		goBack.setContentAreaFilled(false);
 	
 		
 		switch(level){
 			case 2: addMenu3(misma);
 			case 1: addMenu2(misma);
-			case 0: addMenu1(misma);
+			case 0: addMenu1(misma, level);
 				break;
 		}
 	}
 	
-	public void addMenu1 (Misma misma){
+	public void addMenu1 (Misma misma, int level){
 		menu1.setAlignmentX(CENTER_ALIGNMENT);
 		menu1.addActionListener(new ActionListener(){
 			@Override
@@ -60,9 +66,18 @@ public class Menu extends JPanel{
 			}
 		});
 		
+		goBack.setAlignmentX(CENTER_ALIGNMENT);
+		goBack.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				misma.loadMenu(level);
+			}
+		});
+		
 		add(menu1);
 		add(menu2);
 		add(menu5);
+		add(goBack);
 	}
 	
 	public void addMenu2(Misma misma){
