a0aafa5806825b3dec872054128ad6bfa726d937
MadameLatach <frida_isaryd@hotmail.com>
2017-05-11 11:33:07 +0200

    User

--- src/data/FuelData.java
+++ src/data/FuelData.java
@@ -6,11 +6,13 @@ public class FuelData extends DataTransferObject {
 	private double volume;
 	private double emissionRate;
 	private double emission;
-	
-	public FuelData(double volume, double emissionRate, double emission){
+	private String user;
+
+	public FuelData(double volume, double emissionRate, double emission, String user) {
 		this.volume = volume;
 		this.emissionRate = volume;
 		this.emission = emission;
+		this.user = user;
 	}
 
 	public double getVolume() {
@@ -36,6 +38,13 @@ public class FuelData extends DataTransferObject {
 	public void setEmission(double emission) {
 		this.emission = emission;
 	}
-	
-	
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
 }
