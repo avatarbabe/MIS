54e05b726b7fbc190d0eb652f9af6bed5a0c2119
avatarbabe <marso083@student.liu.se>
2017-05-15 13:56:35 +0200

    bb

--- src/data/RouteData.java
+++ src/data/RouteData.java
@@ -77,4 +77,12 @@ public class RouteData extends DataTransferObject {
 		this.emission = emission;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
