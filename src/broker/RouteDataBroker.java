1ac7564f36ea6e0cd360230ad812c5ed82693f0b
MadameLatach <frida_isaryd@hotmail.com>
2017-05-17 12:15:45 +0200

    date

--- src/broker/RouteDataBroker.java
+++ src/broker/RouteDataBroker.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import data.FuelData;
@@ -22,17 +26,22 @@ public class RouteDataBroker extends Broker {
 	}
 
 	public void insert(DataTransferObject data) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate localDate = LocalDate.now();
+		Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
 
 		try {
 			Connection conn = super.getDBConnection();
-			PreparedStatement insertRoutes = conn
-					.prepareStatement("INSERT INTO routes(start, end, distance, emission, username) VALUES(?,?,?,?,?)");
+			PreparedStatement insertRoutes = conn.prepareStatement(
+					"INSERT INTO routes(start, end, distance, emission, username, date) VALUES(?,?,?,?,?)");
 
 			insertRoutes.setString(1, ((RouteData) data).getStart());
 			insertRoutes.setString(2, ((RouteData) data).getEnd());
 			insertRoutes.setDouble(3, ((RouteData) data).getDistance());
 			insertRoutes.setDouble(4, ((RouteData) data).getEmission());
 			insertRoutes.setString(5, ((RouteData) data).getUser());
+			insertRoutes.setDate(5, sqlDate);
 
 			insertRoutes.executeUpdate();
 
@@ -75,7 +84,7 @@ public class RouteDataBroker extends Broker {
 			updateRoutes.setDouble(4, ((RouteData) data).getEmission());
 			updateRoutes.setString(5, ((RouteData) data).getUser());
 			updateRoutes.setInt(6, ((RouteData) data).getId());
-			
+
 			updateRoutes.executeUpdate();
 
 		} catch (SQLException e) {
@@ -84,12 +93,12 @@ public class RouteDataBroker extends Broker {
 		}
 
 	}
-	public void delete(DataTransferObject data){
+
+	public void delete(DataTransferObject data) {
 		Connection conn = super.getDBConnection();
 		try {
-			PreparedStatement deleteRoute = conn.prepareStatement(
-					"DELETE from routes WHERE route_id = ?");
-			
+			PreparedStatement deleteRoute = conn.prepareStatement("DELETE from routes WHERE route_id = ?");
+
 			deleteRoute.setInt(1, ((RouteData) data).getId());
 			deleteRoute.executeUpdate();
 
